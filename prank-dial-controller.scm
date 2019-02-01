3d603a7e4b7fb9203f6ae77a062e73af98c24f33
nobody246 <a.se8seven@gmail.com>
2019-01-31 20:33:50 -0500

    Update prank-dial-controller.scm

--- prank-dial-controller.scm
+++ prank-dial-controller.scm
@@ -1,24 +1,29 @@
 (use posix)
 
-(define zz 120)
-(define pid #f)
-(define (reset)
-  (process "kill"
-           `("-9"
-             ,pid
-             "$(cat /run/user/1000/pulse/pid)"))
-  (process "pulseaudio" `("-D")))
-(define (x)
-  (condition-case
-      ((lambda() 
-         (when pid
-           (reset))
-         (define-values (i o pid)
-           (process "csi" `("prank-dial.scm")))
-         (sleep zz)))
-    ((exn)
-     (print "error in controller. trying again.")
-     (reset)))
-  (x))
-(x)
+(letrec
+    ((zz 16)
+     (pid #f)
+     (reset
+      (lambda()
+        (process
+         "kill"
+         `("-9"
+           ,pid
+           "$(cat /run/user/1000/pulse/pid)"))
+        (process
+         "pulseaudio" `("-D")))))
+    (define (x)
+      (condition-case
+          ((lambda() 
+             (when pid
+               (reset))
+             (define-values
+               (i o pid)
+               (process "csi" `("scambc.scm")))
+             (sleep zz)))
+        ((exn)
+         (print "error in controller. trying again.")
+         (reset)))
+      (x))
+    (x))
   
